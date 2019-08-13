3a544b2edaf9a18a762237828b0bb11f269ab225
matheus <matheusmendess90@gmail.com>
2019-08-12 22:45:31 -0300

    Migrate to androidx

--- app/src/main/java/com/mendev/sportsmanager/shared/BaseActivity.kt
+++ app/src/main/java/com/mendev/sportsmanager/shared/BaseActivity.kt
@@ -1,6 +1,6 @@
 package com.mendev.sportsmanager.shared
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
 
